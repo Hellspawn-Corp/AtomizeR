7ce68f2b12c415f836ece24f4571cf1f6c9f4594
Anna Thorne <annathorne@pm.me>
2025-05-24 23:51:04 +0200

    improve template

--- src/logic/templating.rs
+++ src/logic/templating.rs
@@ -9,9 +9,15 @@ use uuid::Uuid;
 pub fn create_feed_json_template() -> io::Result<()> {
     let template = JsonEntries {
         entries: vec![JsonEntry {
-            id: Uuid::new_v4().to_string(),
+            id: Uuid::new_v4(),
             title: "Entry Title".to_string(),
+            url: "https://www.youtube.com/watch?v=QH2-TGUlwu4".to_string(),
+            published: chrono::Utc::now().with_timezone(&chrono::FixedOffset::east_opt(0).unwrap()),
+            updated: chrono::Utc::now().with_timezone(&chrono::FixedOffset::east_opt(0).unwrap()),
+            summary: "Test".to_string(),
             content: "Hello everybody, my name is Markiplier and today we're going to be playing SCP Containment Breach: My Little Pony.".to_string(),
+            // Set published and updated to current date/time
+            hash: "d41d8cd98f00b204e9800998ecf8427e".to_string(),
         }],
     };
 
--- src/model/json/json_entry.rs
+++ src/model/json/json_entry.rs
@@ -1,7 +1,14 @@
+use chrono::{DateTime, FixedOffset};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 #[derive(Serialize, Deserialize, Debug)]
 pub struct JsonEntry {
-    pub id: String,
+    pub id: Uuid,
     pub title: String,
+    pub url: String,
+    pub published: DateTime<FixedOffset>,
+    pub updated: DateTime<FixedOffset>,
+    pub summary: String,
     pub content: String,
+    pub hash: String,
 }
