a40c683e6a049f76ff5221b52d048262e0a633db
Effie2096 <havealittlefaith2096@gmail.com>
2025-05-24 22:13:49 +0100

    generic base entry and atom entry strucs

--- src/model/atom/atom_entry.rs
+++ src/model/atom/atom_entry.rs
@@ -1,10 +1,65 @@
+struct AtomEntry {
+    base: BaseEntry,
+    summary: String,
+}
+
 impl Entry for AtomEntry {
-    fn new(title: String, url: String, summary: String, updated: String) -> Self {
+    fn new(
+        &self,
+        id: Uuid,
+        title: String,
+        url: String,
+        published: DateTime<Utc>,
+        updated: DateTime<Utc>,
+        content: String,
+        hash: String,
+        summary: String,
+    ) -> Self {
         AtomEntry {
-            title,
-            url,
+            entry: Entry::new(id, title, url, published, updated, content, hash),
             summary,
-            updated,
         }
+    }
+
+    fn new(
+        &self,
+        title: String,
+        url: String,
+        published: DateTime<Utc>,
+        updated: DateTime<Utc>,
+        content: String,
+    ) -> Self {
+        AtomEntry {
+            entry: Entry::new(title, url, published, updated, content),
+            summary,
+        }
+    }
+
+    fn get_id(&self) -> Uuid {
+        self.entry.get_id()
+    }
+
+    fn get_title(&self) -> String {
+        self.entry.get_title()
+    }
+
+    fn get_url(&self) -> String {
+        self.entry.get_url()
+    }
+
+    fn get_published(&self) -> DateTime<Utc> {
+        self.entry.get_published()
+    }
+
+    fn get_updated(&self) -> DateTime<Utc> {
+        self.entry.get_updated()
+    }
+
+    fn get_content(&self) -> String {
+        self.entry.get_content()
+    }
+
+    fn get_hash(&self) -> String {
+        self.entry.get_hash()
     }
 }
--- src/model/base/entry.rs
+++ src/model/base/entry.rs
@@ -1,7 +1,107 @@
 trait Entry {
-    fn get_id(&self) -> String;
+    fn new(
+        &self,
+        id: Uuid,
+        title: String,
+        url: String,
+        published: DateTime<Utc>,
+        updated: DateTime<Utc>,
+        content: String,
+        hash: String,
+    ) -> Self;
+    fn new(
+        &self,
+        title: String,
+        url: String,
+        published: DateTime<Utc>,
+        updated: DateTime<Utc>,
+        content: String,
+    ) -> Self;
+    fn get_id(&self) -> Uuid;
     fn get_title(&self) -> String;
     fn get_url(&self) -> String;
-    fn get_date(&self) -> String;
+    fn get_published(&self) -> DateTime<Utc>;
+    fn get_updated(&self) -> DateTime<Utc>;
     fn get_content(&self) -> String;
+    fn get_hash(&self) -> String;
+}
+
+struct BaseEntry {
+    id: Uuid,
+    title: String,
+    url: String,
+    published: DateTime<Utc>,
+    updated: DateTime<Utc>,
+    content: String,
+    hash: String,
+}
+
+impl Entry for BaseEntry {
+    fn new(
+        &self,
+        id: Uuid,
+        title: String,
+        url: String,
+        published: DateTime<Utc>,
+        updated: DateTime<Utc>,
+        content: String,
+        hash: String,
+    ) -> Self {
+        BaseEntry {
+            id,
+            title,
+            url,
+            published,
+            updated,
+            content,
+            hash,
+        }
+    }
+
+    fn new(
+        &self,
+        title: String,
+        url: String,
+        published: DateTime<Utc>,
+        updated: DateTime<Utc>,
+        content: String,
+    ) -> Self {
+        BaseEntry {
+            id: Uuid::new_v4(),
+            title,
+            url,
+            published,
+            updated,
+            content,
+            hash: String::new(),
+        }
+    }
+
+    fn get_id(&self) -> Uuid {
+        self.id
+    }
+
+    fn get_title(&self) -> String {
+        self.title.clone()
+    }
+
+    fn get_url(&self) -> String {
+        self.url.clone()
+    }
+
+    fn get_published(&self) -> DateTime<Utc> {
+        self.published
+    }
+
+    fn get_updated(&self) -> DateTime<Utc> {
+        self.updated
+    }
+
+    fn get_content(&self) -> String {
+        self.content.clone()
+    }
+
+    fn get_hash(&self) -> String {
+        self.hash.clone()
+    }
 }
